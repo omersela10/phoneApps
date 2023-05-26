b6235b4b0d09c11f32523e410a44ea275c92e962
omersela10 <90277998+omersela10@users.noreply.github.com>
2023-05-26 19:23:11 +0300

    Create SMSManager.java

---
+++ src/phoneApplications/SMSManager.java
@@ -0,0 +1,29 @@
+package phoneApplications;
+
+public class SMSManager implements AppHandler, ContactObserver {
+
+	@Override
+	public void onContactRemoved(Contact anyContact) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isContactExists(Contact anyContact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void startApp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printOptions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
